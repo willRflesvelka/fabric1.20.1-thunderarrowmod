41e9201ef32b121972f81e615e201d400e7afdab
willRflesvelka <willriusu.rain@gmail.com>
2024-09-28 04:26:04 +0900

    test

---
+++ src/main/java/com/github/willrflesvelka/thunderarrowmod/ThunderArrowMod.java
@@ -0,0 +1,15 @@
+package com.github.willrflesvelka.thunderarrowmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThunderArrowMod implements ModInitializer {
+	public static final String MOD_ID = "thunderarrowmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/github/willrflesvelka/thunderarrowmod/ThunderArrowModClient.java
@@ -0,0 +1,10 @@
+package com.github.willrflesvelka.thunderarrowmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThunderArrowModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
